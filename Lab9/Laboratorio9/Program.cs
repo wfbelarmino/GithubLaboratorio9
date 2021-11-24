bd2afb0ad3d99583996636b085956be96b22ccb2
wfbelarmino <92883692+wfbelarmino@users.noreply.github.com>
2021-11-24 14:58:26 -0300

    Add files via upload

--- Lab9/Laboratorio9/Program.cs
+++ Lab9/Laboratorio9/Program.cs
@@ -6,14 +6,19 @@ namespace Laboratorio9
 {
     class Program
     {
-       
-        static void Main(string[] args)
-        {
-            Dictionary<int, string> paises = new Dictionary<int, string>(); 
-            paises[44] = "Reino Unido";
-            paises[33] = "França"; paises[55] = "Brasil";
 
-            Console.WriteLine("O código 55 é: {0}", paises[55]); 
+        static void Main(string[] args)
+
+        {
+
+            Dictionary<int, string> paises = new Dictionary<int, string>();
+            paises[44] = "Reino Unido";
+            paises[33] = "França";
+            paises[55] = "Brasil";
+
+
+
+            Console.WriteLine("O código 55 é: {0}", paises[55]);
             foreach (KeyValuePair<int, string> item in paises)
             {
                 int codigo = item.Key; string pais = item.Value;
@@ -21,13 +26,12 @@ namespace Laboratorio9
             }
 
             List<string> listaStrings = new List<string>();
-            listaStrings.Add("Um"); 
-            listaStrings.Add("Hello"); 
-            listaStrings.Add("World"); 
-            Console.WriteLine(listaStrings[0]); 
-            Console.WriteLine(listaStrings[1]); 
-            Console.WriteLine(listaStrings[2]); 
-            listaStrings.Add(0);
+            listaStrings.Add("Um");
+            listaStrings.Add("Hello");
+            listaStrings.Add("World");
+            Console.WriteLine(listaStrings[0]);
+            Console.WriteLine(listaStrings[1]);
+            Console.WriteLine(listaStrings[2]);
 
             Queue<Object> q = new Queue<Object>();
             q.Enqueue(".net framework / core");
@@ -44,6 +48,41 @@ namespace Laboratorio9
             Console.WriteLine(minhaFila.Dequeue());
             Console.WriteLine(minhaFila.Dequeue());
             Console.WriteLine(minhaFila.Dequeue());
+                
+            List<string> ddi = new List<string>();
+            ddi.Add("Brasil");
+            ddi.Add("Reino Unido");
+            ddi.Add("França");
+            Console.WriteLine(ddi[0]);
+            Console.WriteLine(ddi[1]);
+            Console.WriteLine(ddi[2]);
+
+            Console.WriteLine("O DDI é: {0}", ddi[0]);
+            foreach (KeyValuePair<int, string> item in paises)
+            {
+                int codigo = item.Key; string pais = item.Value;
+                Console.WriteLine("Código {0} = {1}", codigo, pais);
+            }
+
+
+            List<float> ListaAcimaMedia = new List<float>();
+            ListaAcimaMedia.Add(10);
+            ListaAcimaMedia.Add(20);
+            ListaAcimaMedia.Add(30);
+            Console.WriteLine(ListaAcimaMedia[0]);
+            Console.WriteLine(ListaAcimaMedia[1]);
+            Console.WriteLine(ListaAcimaMedia[2]);
+
+            //Console.WriteLine("Valor", ListaAcimaMedia[0]);
+
+            foreach (float soma in ListaAcimaMedia) 
+            {
+               Console.WriteLine(soma / 3);
+            
+            }
+
+          
         }
+
     }
 }
